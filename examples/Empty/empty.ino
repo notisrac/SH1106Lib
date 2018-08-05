321b7ecdadfc23cf38369c3089e251543e8d58c9
notisrac <notisrac@gmail.com>
2018-08-05 22:45:22 +0200

    Added empty sketch

---
+++ examples/Empty/empty.ino
@@ -0,0 +1,20 @@
+#include <SH1106Lib.h>
+
+SH1106Lib display;
+
+void setup() {
+	display.initialize();
+	display.clearDisplay();
+}
+
+void loop() {
+	display.fillRect(10, 10, 80, 30, WHITE);
+
+	delay(1000);
+	display.clearDisplay();
+}
+
+/*
+Sketch uses 4366 bytes (13%) of program storage space. Maximum is 32256 bytes.
+Global variables use 33 bytes (1%) of dynamic memory, leaving 2015 bytes for local variables. Maximum is 2048 bytes.
+*/
