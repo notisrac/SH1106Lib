0a0b7d14cd710bcea579a7086a4d6ccfc8600685
notisrac <notisrac@gmail.com>
2018-08-11 17:18:01 +0200

    Updated size info with new version

--- examples/Empty/empty.ino
+++ examples/Empty/empty.ino
@@ -15,6 +15,6 @@ void loop() {
 }
 
 /*
-Sketch uses 4366 bytes (13%) of program storage space. Maximum is 32256 bytes.
+Sketch uses 3570 bytes (11%) of program storage space. Maximum is 32256 bytes.
 Global variables use 33 bytes (1%) of dynamic memory, leaving 2015 bytes for local variables. Maximum is 2048 bytes.
 */
